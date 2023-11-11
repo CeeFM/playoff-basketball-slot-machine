7633aa5b63d76914742d683918f99be33391699a
Colin Matteson <seefm@proton.me>
2023-11-11 17:54:38 -0500

    rendered player picks to matchup page, now working on generating and rendering CPU team before moving onto external API calls

--- src/components/game/Matchup.js
+++ src/components/game/Matchup.js
@@ -1,8 +1,38 @@
+import { useEffect, useState } from "react"
 import { NavBar } from "../nav/NavBar"
+import { Player } from "./Player"
 
 export const Matchup = () => {
+    const [userTeam, setUserTeam] = useState([])
+    const [match, setMatch] = useState([])
+    const team = userTeam.filter((player) => player.matchId === (match.length + 1))
+
+
+    useEffect(
+        () => {
+            fetch('http://localhost:8088/userTeam?_expand=player')
+                .then(response => response.json())
+                .then((userTeamArray) => {
+                    setUserTeam(userTeamArray)
+                })
+        },
+        []
+        ) 
+
+    console.log(team)
     return <>
     <NavBar />
+    {
+        team.map((baller) => {
+            return <>
+            <section className="player-section" id={baller?.player?.externalAPIId}>
+        <strong className="name">{baller?.player?.name}</strong> 
+        <br />
+        <img className="player-img" src={baller?.player?.img} />
+            </section>
+            </>
+        })
+    }
     
     </>
 }
